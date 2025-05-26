601a62bbc32408cd2638304f5ca5f87e7c1136bb
Wonchang0314 <andrew0314@naver.com>
2025-05-27 03:35:59 +0900

    FEAT: Icon 컴포넌트 추가 및 프로젝트 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Page as DashBoard } from "@/pages/DashBoard";
+import { Page as ApplyList } from "@/pages/ApplyList";
+import { Page as Setting } from "@/pages/Setting";
+import { Page as Evaluation } from "@/pages/Evaluation";
+import Header from "@/components/Header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div className="h-screen bg-[#121826]">
+        <Header />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/applies" element={<ApplyList />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/evaluation" element={<Evaluation />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
