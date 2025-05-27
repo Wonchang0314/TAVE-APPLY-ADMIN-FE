c0df141310f5c3d2742926b1128301bfdad0933c
Wonchang0314 <andrew0314@naver.com>
2025-05-28 02:14:14 +0900

    FEAT: Button 컴포넌트 개발

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,26 @@
-interface ButtonProps {
+import type { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   width?: string;
   className?: string;
+  disabled?: boolean;
+  onClick: () => void;
 }
 
-const Button = ({ text, width, className }: ButtonProps) => {
+const Button = ({
+  text,
+  width,
+  className,
+  disabled = false,
+  onClick,
+}: ButtonProps) => {
   return (
     <button
-      className={`px-3 py-4 text-semibold text-white bg-blue-500 rounded-lg ${width} ${className}`}
+      className={`px-4 py-3 text-semibold text-white bg-blue-600 rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-white font-semibold ${width} ${className}`}
+      onClick={onClick}
+      onKeyDown={(e) => e.key === "Enter" && onClick()}
+      disabled={disabled}
     >
       {text}
     </button>
