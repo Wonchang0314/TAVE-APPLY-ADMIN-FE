60bf7467a7e99628dcc8ea67647c39b570b895e2
Wonchang0314 <andrew0314@naver.com>
2025-05-30 07:35:21 +0900

    FEAT: 인증 로직(임시) 추가

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -8,41 +8,63 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// axiosInstance.interceptors.request.use((config) => {
-//   const accessToken = localStorage.getItem("accessToken");
-//   if (accessToken && config.headers) {
-//     config.headers.Authorization = `Bearer ${accessToken}`;
-//   }
-//   return config;
-// });
-
 // axiosInstance.interceptors.request.use(
+//   async (config) => {
+//     if (typeof window !== undefined) {
+//       const token = sessionStorage.getItem("access_token");
+//       config.headers.set("Authorization", `Bearer ${token}`);
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// axiosInstance.interceptors.response.use(
 //   (response) => response,
 //   async (error) => {
-//     const axiosError: AxiosError = error;
-//     const originalRequest = error.config;
-
-//     if (
-//       axiosError.response?.status === 401 &&
-//       !originalRequest.retry &&
-//       axiosError.message === "토큰 재발급이 필요합니다."
-//     ) {
-//       try {
-//         const res = await axios.post(`/v1/auth/refresh`);
-//         const newAccessToken = res.data.accessToken;
-//         localStorage.setItem("accessToken", newAccessToken);
-
-//         originalRequest.headers = {
-//           ...originalRequest.headers,
-//           Authorization: `Bearer ${newAccessToken}`,
-//         };
-
-//         return axiosInstance(originalRequest);
-//       } catch (refreshError) {
-//         console.error("토큰 갱신 실패:", refreshError);
-//         return Promise.reject(refreshError);
+//     if (error.response?.status === 401) {
+//       if (error.response.data.message === "토큰 재발급이 필요합니다.") {
+//         const originalRequest = error.config;
+//         try {
+//           const tokenResponse = await postRefreshToken(
+//             sessionStorage.getItem("email")
+//           );
+//           if (tokenResponse.status === 201) {
+//             const newAccessToken = tokenResponse.data.accessToken;
+//             sessionStorage.setItem("access_token", newAccessToken);
+//             originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+//             return client(originalRequest);
+//           }
+//         } catch (refreshError) {
+//           if (axios.isAxiosError(refreshError)) {
+//             alert("로그인이 필요합니다.");
+//             window.location.replace("/");
+//           }
+//           return Promise.reject(refreshError);
+//         }
 //       }
 //     }
 //     return Promise.reject(error);
 //   }
+// );
+// axiosInstance.interceptors.request.use(
+//   async (config) => {
+//     if (typeof window !== undefined) {
+//       const token = sessionStorage.getItem("access_token");
+//       config.headers.set("Authorization", `Bearer ${token}`);
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// axiosInstance.interceptors.request.use(
+//   async (config) => {
+//     if (typeof window !== undefined) {
+//       const token = sessionStorage.getItem("access_token");
+//       config.headers.set("Authorization", `Bearer ${token}`);
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
 // );
