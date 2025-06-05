aa2c2b4f69615b6c45fa76c40eeba7a53416648b
Wonchang0314 <andrew0314@naver.com>
2025-06-05 21:41:20 +0900

    FIX: msw worker 시작 로직 수정

--- src/api/msw/browser.ts
+++ src/api/msw/browser.ts
@@ -5,10 +5,14 @@ export const worker = setupWorker(...handlers);
 
 // worker 시작
 export const startMSW = async () => {
-  if (process.env.NODE_ENV === "development") {
-    await worker.start({
-      onUnhandledRequest: "bypass", // MSW에서 처리되지 않는 요청은 그대로 통과
-    });
-    console.log("🔶 MSW 모킹 서버가 시작되었습니다.");
-  }
+  // if (process.env.NODE_ENV === "development") {
+  //   await worker.start({
+  //     onUnhandledRequest: "bypass", // MSW에서 처리되지 않는 요청은 그대로 통과
+  //   });
+  //   console.log("🔶 MSW 모킹 서버가 시작되었습니다.");
+  // }
+  await worker.start({
+    onUnhandledRequest: "bypass", // MSW에서 처리되지 않는 요청은 그대로 통과
+  });
+  console.log("🔶 MSW 모킹 서버가 시작되었습니다.");
 };
